5080437896369f74cf33b0b29e2fa4173d3ce947
Terje Sandstrom <terje@osiris.no>
2013-09-19 11:47:34 +0200

    Added git ignore

---
+++ Inmeta.AssociateRecentWorkItems/AssociateRecentWorkItemsPackage.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace Osiris.AssociateRecentWorkItems
+{
+ [PackageRegistration(UseManagedResourcesOnly = true)]
+ [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid(GuidList.guidOsirisAssociateRecentWorkItemPkgString)]
+    public sealed class AssociateRecentWorkItemsPackage : Package
+    {
+    }
+}
