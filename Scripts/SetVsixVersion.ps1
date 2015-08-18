7f94267e2efac42cef09875c138cc5cc1bbceaf5
Jakob Ehn <jakob@ehn.nu>
2015-08-18 11:36:00 +0200

    Changed back to 3 digits version number

--- Scripts/SetVsixVersion.ps1
+++ Scripts/SetVsixVersion.ps1
@@ -3,5 +3,5 @@ $paramFiles=get-childitem "*.vsixmanifest" -rec -Verbose
 foreach ($file in $paramFiles)
 {
     Write-Host -Verbose ("Replacing version in " + $file.PSPath + " with version " + $env:BUILD_BUILDNUMBER)
-	(Get-Content $file.PSPath) | Foreach-Object { $_ -replace "1.0.0.0", "$env:BUILD_BUILDNUMBER" } | Set-Content $file.PSPath
+	(Get-Content $file.PSPath) | Foreach-Object { $_ -replace "1.0.0", "$env:BUILD_BUILDNUMBER" } | Set-Content $file.PSPath
 }
